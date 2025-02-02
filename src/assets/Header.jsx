e52fea73a3ecc7f3daa6af07dda1b0299b7679df
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-02-01 21:01:34 -0500

    basics

---
+++ src/assets/Header.jsx
@@ -0,0 +1,17 @@
+
+
+ function Header(){
+      
+    return(
+            <header>
+                     <h1>My Website</h1>
+                     <ul>
+                          <li>Home</li>
+                          <li>About</li>
+                          <li>Services</li>
+                     </ul>
+            </header>
+    );
+ }
+
+ export default Header
