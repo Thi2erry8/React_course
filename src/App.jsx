87474db79ee8af49b511e569a00e5f11e16df6f5
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-02-03 17:51:12 -0500

    props

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   
   return(
     <>
-      <Student name="Spongebob" age={30} isStudent={true}/> 
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Ptrick" age={45} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/> 
     </>
   );
 }
