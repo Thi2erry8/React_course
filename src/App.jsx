46b6e7207c574b57a041c527d2d663141704d20e
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-02-03 22:18:00 -0500

    Conditional_rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Student from './Students.jsx'
+import UserGreeting from './assets/UserGreeting'
 
 function App() {
   
   return(
     <>
-      <Student name="Spongebob" age={30} isStudent={true}/>
-      <Student name="Ptrick" age={45} isStudent={false}/>
-      <Student name="Squidward" age={50} isStudent={false}/> 
-       <Student/>
+        <UserGreeting isLoggedIn={true} username = "Brocode"/>
     </>
   );
 }
