4aab3f0f506d183f6105c49283f452e960fd32f4
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-02-22 21:33:43 -0500

    end

--- src/assets/MyComponent.jsx
+++ src/assets/MyComponent.jsx
@@ -2,11 +2,36 @@ import React, { useState , useEffect } from "react";
 
 function MyComponent(){
             
+const [time, setTime] = useState(new Date());
 
+useEffect(() => {
+    const intervalId = setInterval(() => {
+        setTime(new Date());
+    },1000);
+    return () => {
+      clearInterval(intervalId);
+    }
+});
 
-return(<>
+function formatTime(){ 
+    let hours = time.getHours() ;
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    const meridian = hours >= 12 ? "PM" : "AM";
 
-      </>);
+    hours = hours % 12 || 12 ;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`
+};
+
+function padZero(number){
+      return (number < 10 ? "0" : "") + number;
+}
+return(<div className="clock-container">
+              <div className="clock">
+                    <span>{formatTime()}</span>
+              </div>
+      </div>);
 
 };
 export default MyComponent
